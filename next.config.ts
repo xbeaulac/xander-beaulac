d378b9dbc0eaba9a157f261850a7ed52102a8abf
Xander Beaulac <xbeaulac@gmail.com>
2025-08-03 16:09:33 -0700

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination:
+          "https://docs.google.com/document/d/16AFFCZI-t5xr9_4XJtL9KRd-Lrt2qV9i/edit?usp=sharing&ouid=111632038240813900288&rtpof=true&sd=true",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
