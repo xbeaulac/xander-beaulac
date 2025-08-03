e3a9d1f9ab77d6f1f32bb54492b037b96db82714
Xander Beaulac <xbeaulac@gmail.com>
2025-08-03 16:16:27 -0700

    metadata fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return null;
+  redirect(
+    "https://docs.google.com/document/d/16AFFCZI-t5xr9_4XJtL9KRd-Lrt2qV9i/edit?usp=sharing&ouid=111632038240813900288&rtpof=true&sd=true"
+  );
 }
